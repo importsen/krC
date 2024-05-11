432a42620a461ea1abb0806a599ad1ff39dc72a0
sen <senecayung@gmail.com>
2024-05-10 21:34:15 -0400

    completed 1.2

--- ch-1_2-exe.c
+++ ch-1_2-exe.c
@@ -4,4 +4,28 @@
  * 1-4: write a program to print the corresponding celsius to fahrenheit
  */
 
+#include <stdio.h>
+
+/* print celsius-fahr table
+ * for fhar = 0, 20, ..., 300; floating point version */
+
+main()
+{
+    float celsius, fahr;
+    float lower, upper, step;
+
+    lower = 0;		/* lower limit of the temp table */
+    upper = 300;	/* upper limit of the temp table */
+    step = 20;		/* step size */
+
+    printf("celsius to fahrenheit table\n");
+
+    celsius = lower;
+    while (celsius <= upper) {
+	    fahr = (celsius) * (9.0/5.0) + 32 ;
+	    printf("%6.1f %3.0f\n", celsius, fahr);
+	    celsius = celsius + step;
+	}
+}
+
 
